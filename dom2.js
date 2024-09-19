22de73493b05ce257bd26b6aa123c288f2676d29
Aik-202 <ikogweassurance85@gmail.com>
2024-09-19 14:21:52 +0100

    js statment covered

--- dom2.js
+++ dom2.js
@@ -2,11 +2,11 @@
 // the css of an element, it can also change 
 // it's attributes
 
-light = document.getElementById("light")
-nolight = document.getElementById("nolight")
+light = document.getElementById("light");
+nolight = document.getElementById("nolight");
 
-switchOnButton = document.getElementById("switchOnButton")
-switchOffButton = document.getElementById("switchOffButton")
+switchOnButton = document.getElementById("switchOnButton");
+switchOffButton = document.getElementById("switchOffButton");
 
 function switchOnLight () {
     nolight.setAttribute("src", "light.png")
