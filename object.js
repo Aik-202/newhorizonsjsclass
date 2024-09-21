87d7856a745bee175dcd86c21702ea21eb552a26
Aik-202 <ikogweassurance85@gmail.com>
2024-09-21 15:02:23 +0100

    Teaching Objects

---
+++ object.js
@@ -0,0 +1,22 @@
+// Reallife Objects
+// In real life it's simply and item with 
+// different properties
+
+// For example a car
+// A model.. bmw, toyota, honda, rangeover
+// A color..  blue, green, red, black
+// type: jeep, bus, suv
+
+// Objects in js is similiar... it's takes the 
+// property name and the property value and groups them
+// It groups them using {}
+
+let car = {model: "bmw", color:"black", type:"suv"}
+
+// Accessing Object Properties
+// objectname.propertyname
+console.log(car.model)
+
+// OR
+// objectname["propertyname"]
+console.log(car["color"])
