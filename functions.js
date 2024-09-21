2b116c9a8b9605b028b66d9d949cade21e11432b
Aik-202 <ikogweassurance85@gmail.com>
2024-09-21 14:18:43 +0100

    Teaching functions

---
+++ functions.js
@@ -0,0 +1,49 @@
+// Javascript Function
+
+// A function is a block of code that performs a specific task
+
+// Facts about Functions
+
+// - A function in js must start with the keyword function 
+// follwed by it's name then () then {}
+
+// - A function needs to be called for it to be used, 
+// this is what we call invocation
+// We call it by it's name follwed by ()
+
+// - A function can have parameters
+// Parameter is the value a function receives
+// The parameter goes in the ()
+// e.g hideText(a)
+// e.g hideText(a, b)
+
+// - A function can return a value
+// we use th keyword return to return a value
+
+// WHY FUNCTIONS?
+// Functions are reusable
+// With Functions we can use the same code, just with different inputs
+
+// A simple calculator
+// Add, Sub, Multiply, Division
+
+let a = 14
+let b = 7
+
+// Addition
+function add(a, b) {
+    c = a + b
+    return c // signifies the end of a function
+}
+
+// Subtraction
+sub = a - b
+console.log(sub)
+
+// Multiplication
+mul = a * b
+console.log(mul)
+
+// Division
+div = a / b
+console.log(div)
