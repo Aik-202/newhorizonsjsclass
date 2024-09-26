6e7deb6aaac33359c47e2d950157ec030265142e
Aik-202 <ikogweassurance85@gmail.com>
2024-09-26 14:08:17 +0100

    Done with events

---
+++ jsevents.js
@@ -0,0 +1,24 @@
+// What is an Event?
+// An event simply put refers to the thing 
+// that happens to an HTML element
+
+// JS now help us to react to those events
+// This is what we call DOM Manipulation
+
+// HTML EVENTS
+// Page is loading => onload
+// Input field is changed => oninputchange
+// Button is clicked => onclick
+// Mouse moves over an element => onmouseover
+// Mouse leaves an element => onmouseleave
+
+// Event Handlers
+// JS functions that reacts to these events are 
+// called event handlers
+
+// <button onclick="hideText()" id="hidebutton">
+// Event => what is happening to the button
+// Clicked, onclick
+// Event Handler => hideText()
+// It determines what now happens when the 
+// button is clicked 
